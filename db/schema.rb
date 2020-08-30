7ecf59a709ac0aa3fdf840cc2d08f417765193c4
chucoyos <manzanillo91@hotmail.com>
2020-08-30 00:13:17 -0500

    Iteration D1 Finding a Cart p138

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_181209) do
+ActiveRecord::Schema.define(version: 2020_08_30_045050) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
